4870e6a00e5e360acc435d35c4dfa70036af9dc9
kens93207 <kens93207@gmail.com>
2025-05-08 06:14:54 +0800

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import {
   BrowserRouter,
   Route,
   Routes,
-  Navigate
+
 } from "react-router-dom"
 import '@ant-design/v5-patch-for-react-19';
-import HomePage from './pages/HomePage.jsx'
+import HomePage from './pages/HomePage.jsx' 
 import CheckoutPage from './pages/CheckoutPage'
 import CartPage from './pages/CartPage'
 import ProductCollectionPage from './pages/ProductCollectionPage'
@@ -15,31 +15,28 @@ import PageTitle from './components/layout/PageTitle'
 import { AuthProvider}  from './components/auth/AuthContext'
 import JumpToTop from './components/common/JumpToTop.jsx'
 import ScrollToTop from 'react-scroll-to-top'
-import LoginPage from './pages/LoginPage.jsx'
-
+import LoginPage from './pages/LoginPage.jsx' 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    
     <AuthProvider>
-    <BrowserRouter basename="/ShopeePractice">
-    <JumpToTop />
-    <ScrollToTop />
-      <PageTitle />
-      <Routes>
-        <Route path="/" element={<Navigate to="/mall" />} />
-        <Route path="/mall" element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/mall/:categoryName" element={<ProductCollectionPage />} />
-        <Route path="/:product" element={<ProductPage />} />
-        <Route path="/login" element={<LoginPage/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/ShopeePractice">
+        <JumpToTop />
+        <ScrollToTop smooth component={<p style={{ color: "blue" }}>UP</p>} />
+        <PageTitle />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/mall" element={<HomePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/mall/:categoryName" element={<ProductCollectionPage />} />
+          <Route path="/:product" element={<ProductPage />} />
+          <Route path="/login" element={<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
-    
   )
 }
 
